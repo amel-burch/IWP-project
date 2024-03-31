5110e66a9617c95215101026f22b0c29f64aa171
amel-burch <amel.karsic@stu.ibu.edu.ba>
2024-03-31 04:31:10 -0500

    added login and registration page

---
+++ login and reg/login.js
@@ -0,0 +1,20 @@
+/*
+
+inspiration: 
+https://dribbble.com/shots/2292415-Daily-UI-001-Day-001-Sign-Up
+
+*/
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.location.href = "C:/Users/USER/source/repos/IWP-project/index.html";
+});
+
+let register = document.getElementById('register');
+register.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.l
+  window.location.href = "C:/Users/USER/source/repos/IWP-project/login and reg/registration.html";
+});
---
+++ login and reg/registration.js
@@ -0,0 +1,6 @@
+let form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location.href = "C:/Users/USER/source/repos/IWP-project/login and reg/login.html";
+  });
